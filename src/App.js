c656dbafe3c02b035d38d7b29e28cadbbfcfc971
mPooja-15 <mishrapooja8128@gmail.com>
2024-08-05 09:54:36 +0530

    Created sign up and sign in page UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import "./index.css";
 
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+// import SignUp from "./pages/Signup";
+
 function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-500">Hello!!</h1>
-    </div>
+    <Router>
+      <div className="min-h-screen flex items-center justify-center bg-gray-100">
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          {/* <Route path="/signup" element={<SignUp />} /> */}
+          <Route
+            path="/"
+            element={
+              <h1 className="text-4xl font-bold text-blue-500">Hello!!</h1>
+            }
+          />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
