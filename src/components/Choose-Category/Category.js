af068eebd3ef95abc59c2cf32dd741f87bed092b
mPooja-15 <mishrapooja8128@gmail.com>
2024-09-02 15:33:56 +0530

    build error issue fix

--- src/components/Choose-Category/Category.js
+++ src/components/Choose-Category/Category.js
@@ -14,115 +14,22 @@ import accesories from '../../assets/settingsModalSvg/accesories.png';
 import pet from '../../assets/settingsModalSvg/pet.png';
 import vaca from '../../assets/settingsModalSvg/vaca.png';
 import job from '../../assets/settingsModalSvg/job.png';
+import { category } from "../../utils";
 
 const Category = () => {
 
     return (
         <div className="p-[22px]">
             <div className="text-[#000] font-semibold text-[20px] mb-4">Choose Category</div>
-            <div className=" cursor-pointer hover:translate-x-2 duration-200 flex mb-[10px] items-center justify-between">
+            {category.map((items, index) => (
+                <div key={index} className=" cursor-pointer hover:translate-x-2 duration-200 flex mb-[10px] items-center justify-between">
                 <div className="flex gap-[1rem] items-center">
-                    <Image src={Cars} />
-                    <h1 className="text-[16px] font-medium text-[#070000]">Cars & Vechiles</h1>
+                    <Image alt="images" src={items?.image} />
+                    <h1 className="text-[16px] font-medium text-[#070000]">{items?.title}</h1>
                 </div>
                 <FaChevronRight className="text-[]" />
             </div>
-
-            <div className=" cursor-pointer hover:translate-x-2 duration-200 flex mb-[15px] items-center justify-between">
-                <div className="flex gap-[1rem] items-center">
-                    <Image src={property} />
-                    <h1 className="text-[16px] font-medium text-[#070000]">Property</h1>
-                </div>
-                <FaChevronRight className="text-[]" />
-            </div>
-
-            <div className=" cursor-pointer hover:translate-x-2 duration-200 flex mb-[15px] items-center justify-between">
-                <div className="flex gap-[1rem] items-center">
-                    <Image src={Mobile} />
-                    <h1 className="text-[16px] font-medium text-[#070000]">Mobile</h1>
-                </div>
-                <FaChevronRight className="text-[]" />
-            </div>
-
-            <div className=" cursor-pointer hover:translate-x-2 duration-200 flex mb-[15px] items-center justify-between">
-                <div className="flex gap-[1rem] items-center">
-                    <Image src={Electronics} />
-                    <h1 className="text-[16px] font-medium text-[#070000]">Electronics</h1>
-                </div>
-                <FaChevronRight className="text-[]" />
-            </div>
-
-            <div className=" cursor-pointer hover:translate-x-2 duration-200 flex mb-[15px] items-center justify-between">
-                <div className="flex gap-[1rem] items-center">
-                    <Image src={Books} />
-                    <h1 className="text-[16px] font-medium text-[#070000]">Books</h1>
-                </div>
-                <FaChevronRight className="text-[]" />
-            </div>
-
-            <div className=" cursor-pointer hover:translate-x-2 duration-200 flex mb-[15px] items-center justify-between">
-                <div className="flex gap-[1rem] items-center">
-                    <Image src={bikes} />
-                    <h1 className="text-[16px] font-medium text-[#070000]">Bikes</h1>
-                </div>
-                <FaChevronRight className="text-[]" />
-            </div>
-
-            <div className=" cursor-pointer hover:translate-x-2 duration-200 flex mb-[15px] items-center justify-between">
-                <div className="flex gap-[1rem] items-center">
-                    <Image src={Sports} />
-                    <h1 className="text-[16px] font-medium text-[#070000]">Sports</h1>
-                </div>
-                <FaChevronRight className="text-[]" />
-            </div>
-
-            <div className=" cursor-pointer hover:translate-x-2 duration-200 flex mb-[15px] items-center justify-between">
-                <div className="flex gap-[1rem] items-center">
-                    <Image src={Homedecor} />
-                    <h1 className="text-[16px] font-medium text-[#070000]">Home decor  & Furniture</h1>
-                </div>
-                <FaChevronRight className="text-[]" />
-            </div>
-
-            <div className=" cursor-pointer hover:translate-x-2 duration-200 flex mb-[15px] items-center justify-between">
-                <div className="flex gap-[1rem] items-center">
-                    <Image src={Clothing} />
-                    <h1 className="text-[16px] font-medium text-[#070000]">Clothing</h1>
-                </div>
-                <FaChevronRight className="text-[]" />
-            </div>
-
-            <div className=" cursor-pointer hover:translate-x-2 duration-200 flex mb-[15px] items-center justify-between">
-                <div className="flex gap-[1rem] items-center">
-                    <Image src={accesories} />
-                    <h1 className="text-[16px] font-medium text-[#070000]">Accessories</h1>
-                </div>
-                <FaChevronRight className="text-[]" />
-            </div>
-
-            <div className=" cursor-pointer hover:translate-x-2 duration-200 flex mb-[15px] items-center justify-between">
-                <div className="flex gap-[1rem] items-center">
-                    <Image src={pet} />
-                    <h1 className="text-[16px] font-medium text-[#070000]">Pets</h1>
-                </div>
-                <FaChevronRight className="text-[]" />
-            </div>
-
-            <div className=" cursor-pointer hover:translate-x-2 duration-200 flex mb-[15px] items-center justify-between">
-                <div className="flex gap-[1rem] items-center">
-                    <Image src={vaca} />
-                    <h1 className="text-[16px] font-medium text-[#070000]">Vacation</h1>
-                </div>
-                <FaChevronRight className="text-[]" />
-            </div>
-
-            <div className=" cursor-pointer hover:translate-x-2 duration-200 flex mb-[5px] items-center justify-between">
-                <div className="flex gap-[1rem] items-center">
-                    <Image src={job} />
-                    <h1 className="text-[16px] font-medium text-[#070000]">Jobs</h1>
-                </div>
-                <FaChevronRight className="text-[]" />
-            </div>
+            ))}
         </div>
     )
 }
--- src/pages/settings/youraddresses.jsx
+++ src/pages/settings/youraddresses.jsx
@@ -17,35 +17,35 @@ const Youraddresses = () => {
           <div className="text-[18px] font-medium text-center">3-4 days</div>
         </div>
         <div>
-          <Image src={imageas} className="" alt="" />
+          <Image src={imageas} className="" alt="images" />
         </div>
         <div className="p-5 ">
           <div className="font-bold">Delivery Tracking</div>
           <div className="flex gap-2 flex-col mt-3">
             <div className="flex items-center gap-[0.5rem]">
-            <Image src={Clock} width={20} height={20} />
+            <Image alt="images" src={Clock} width={20} height={20} />
               <h1 className="text-[#9095A0]">Order Placed</h1>
             </div>
            <div className=" p-1 flex flex-col">
-           <Image src={Vector} className="mb-[5px]" width={7} height={7} />
-            <Image src={Vector} className="mb-[5px]" width={7} height={7} />
-            <Image src={Vector} className="mb-[9px]" width={7} height={7} />
+           <Image alt="images" src={Vector} className="mb-[5px]" width={7} height={7} />
+            <Image alt="images" src={Vector} className="mb-[5px]" width={7} height={7} />
+            <Image alt="images" src={Vector} className="mb-[9px]" width={7} height={7} />
 
-            <Image src={Vector} className="mb-[5px]" width={7} height={7} />
-            <Image src={Vector} className="mb-[5px]" width={7} height={7} />
-            <Image src={Vector} className="" width={7} height={7} />
+            <Image alt="images" src={Vector} className="mb-[5px]" width={7} height={7} />
+            <Image alt="images" src={Vector} className="mb-[5px]" width={7} height={7} />
+            <Image alt="images" src={Vector} className="" width={7} height={7} />
 
            </div>
 
            <div className="flex items-center mb-[35px] gap-[0.5rem]">
-            <Image src={Deliveryfast } width={20} height={20} />
+            <Image alt="images" src={Deliveryfast } width={20} height={20} />
               <div>
               <h1 className=" text-[14px] font-normal text-[#9095A0]">In Transit</h1>
               <p className=" font-normal text-[#000000] text-[8px]">Tracking id #4651418463</p>
               </div>
             </div>
             <div className="flex items-center p-[2px] gap-[0.5rem]">
-            <Image src={Deliveryfast } width={20} height={20} />
+            <Image alt="images" src={Deliveryfast } width={20} height={20} />
               <div>
               <h1 className=" text-[14px] font-normal text-[#9095A0]">Deliverd</h1>
               <p className=" font-normal text-[#000000] text-[18px]">201 Katlian No.21 Street</p>
