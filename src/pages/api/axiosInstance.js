ee7c40c3853050dc120a15018a97ad23a413dd43
Ravi Sharma <ravi701107@gmail.com>
2024-09-16 13:39:28 +0530

    change api calling from insecure connection

--- src/pages/api/axiosInstance.js
+++ src/pages/api/axiosInstance.js
@@ -1,12 +1,19 @@
 // axiosInstance.js
 import axios from "axios";
+import http from "http";
+
+const httpAgent = new http.Agent({
+  rejectUnauthorized: false,
+});
 
 const axiosInstance = axios.create({
   // baseURL: "http://localhost:1008/api/v1/", // Replace with your API base URL
-  baseURL: "http://sixapp-env.eba-bjytpnjh.eu-north-1.elasticbeanstalk.com/api/v1/", // Replace with your API base URL
+  baseURL:
+    "http://sixapp-env.eba-bjytpnjh.eu-north-1.elasticbeanstalk.com/api/v1/", // Replace with your API base URL
   headers: {
     "Content-Type": "application/json",
   },
+  httpAgent: httpAgent,
 });
 
 export default axiosInstance;
