c9d08d7aaea9e9365d596f396809cd7d597f57c2
sybil-lang <78490279+sybil-lang@users.noreply.github.com>
2024-10-18 19:45:56 +0530

    working on responsiveness and started the api integration

--- src/pages/api/axiosInstance.js
+++ src/pages/api/axiosInstance.js
@@ -9,7 +9,8 @@ const httpAgent = new http.Agent({
 const axiosInstance = axios.create({
   // baseURL: "http://localhost:1008/api/v1/", // Replace with your API base URL
   baseURL:
-    "http://sixapp-env.eba-bjytpnjh.eu-north-1.elasticbeanstalk.com/api/v1/", // Replace with your API base URL
+    "http://13.234.115.173:8000/api/v1/", // Replace with your API base URL
+    // "http://sixapp-env.eba-bjytpnjh.eu-north-1.elasticbeanstalk.com/api/v1/", // Replace with your API base URL
   headers: {
     "Content-Type": "application/json",
   },
